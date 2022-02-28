b7640f6d97c0a1685023d4c617970ea1753a76b5
Jordan Ribbink <jribbink@telus.net>
2022-02-27 19:12:10 -0800

    Added chat functionality

---
+++ src/renderer/components/molecules/ChatBar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="d-flex flex-row">
+        <input type="text" class="flex-grow-1" v-model="messageBody" @keypress.enter="sendMessage" />
+        <button class="btn btn-primary" @click="sendMessage" :disabled="sendDisabled">Send</button>
+    </div>
+</template>
+
+<script lang="ts">
+import Message from 'renderer/models/message'
+import { mapMutations } from 'vuex'
+export default {
+    data() {
+        return {
+            messageBody: ""
+        }
+    },
+    methods: {
+        ...mapMutations(['addMessage']),
+        sendMessage() {
+            if(!this.messageBody) return;
+            this.addMessage(new Message(new Date(), "Jordan", this.messageBody))
+            this.messageBody = ""
+        }
+    },
+    computed: {
+        sendDisabled(): boolean {
+            return this.messageBody == ""
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/renderer/components/molecules/ChatBox.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="p-2">
+        <chat-message v-for="(message, index) in messages" :message="message" :key="index"></chat-message>
+    </div>
+</template>
+
+<script lang="ts">
+import ChatMessage from "~/components/atoms/ChatMessage.vue"
+import { mapGetters } from 'vuex'
+
+export default {
+    mounted() {
+        console.log(this.messages)
+    },
+    data() {
+        return {}
+    },
+    computed: {
+        ...mapGetters({
+            messages: 'getMessages'
+        })
+    },
+    components: {
+        ChatMessage
+    }
+}
+</script>
