44fb7ee056ecc84b508b5e6cef8a7b69308f2f9c
Gabriel McLachlan <75510344+GMcLachlan45@users.noreply.github.com>
2022-03-14 14:12:32 -0700

    Add comments to Agent to prepare before branching

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -1,3 +1,19 @@
 class Agent:
     def query(self, query) -> str:
+
+        #TODO: Spelling Check, call a function within agent to fix the query to realistic words --GABE or whoever gets to it
+
+        #TODO Part of speach tagging --Nathan
+
+        #TODO: Named Entity Recognition: Recognize names given and append 
+        #saying "hello" or "tell jessica to" or something to the front --GABE
+
+        #TODO: COReference: Figure out if the query is about the user or their patient is talking about --Jordan C
+
+
+        ##TODO??? Other things to cover our bases if it's easy
+
+        ####TODODODO: Add all of the sections, and return Dr phils smart answer to the query all 3
+
+
         return "hello bro!"
