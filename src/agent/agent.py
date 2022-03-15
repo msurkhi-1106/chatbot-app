a2cfab35b6fd013030355726480e4c9dcf6fcd96
Nathan Wright <nwright567@outlook.com>
2022-03-14 17:05:28 -0700

    Created a seperate method for POS-tagging

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -5,12 +5,9 @@ class Agent:
         #TODO: Spelling Check, call a function within agent to fix the query to realistic words --GABE or whoever gets to it
 
         #TODO Part of speach tagging --Nathan
+        query = self.pos_tag(query)
         
-        query = nltk.word_tokenize(query)
-        query = nltk.pos_tag(query)
-        
-        #TODO: Named Entity Recognition: Recognize names given and append 
-        #saying "hello" or "tell jessica to" or something to the front --GABE
+        #TODO: Named Entity Recognition: Recognize names given and append
         
         #TODO: COReference: Figure out if the query is about the user or their patient is talking about --Jordan C
 
@@ -20,3 +17,9 @@ class Agent:
         ####TODODODO: Add all of the sections, and return Dr phils smart answer to the query all 3
 
         return query
+    
+    def pos_tag(self, query):
+        token = nltk.word_tokenize(query)
+        tagged = nltk.pos_tag(token)
+        
+        return tagged
