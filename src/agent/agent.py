a26f0edc573edf418697b131edbf795fecd1865c
jcoll059 <jordancolledge059@gmail.com>
2022-03-14 21:27:31 -0700

    Added synonyms method to agent.py

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -1,9 +1,11 @@
 import nltk
+nltk.download('punkt')
+nltk.download('popular')
+from nltk.corpus import wordnet
 class Agent:
     def query(self, query) -> str:
-
         #TODO: Spelling Check, call a function within agent to fix the query to realistic words --GABE or whoever gets to it
-
+        syns = self.synonyms(query)
         #TODO Part of speach tagging --Nathan
         query = self.pos_tag(query)
         
@@ -16,6 +18,7 @@ class Agent:
 
         ####TODODODO: Add all of the sections, and return Dr phils smart answer to the query all 3
 
+        
         return query
     
     def pos_tag(self, query):
@@ -23,3 +26,18 @@ class Agent:
         tagged = nltk.pos_tag(token)
         
         return tagged
+
+    def synonyms(self, word):
+        tag = self.pos_tag(word)
+        synonyms = []
+        for syn_set in wordnet.synsets(word):
+            for l in syn_set.lemmas():
+                name = l.name().replace("_", " ")
+                testTag = self.pos_tag(name)
+                if testTag[0][1] == tag[0][1]:
+                    name = name.lower()
+                    synonyms.append(name)
+        
+        print(set(synonyms))
+
+        return synonyms
