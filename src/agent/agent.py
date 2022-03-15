b4978b18a77125de2217a39f11e9cf7b8d3ac434
jcoll059 <jordancolledge059@gmail.com>
2022-03-14 21:38:54 -0700

    Synonyms method catches errors

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -27,17 +27,26 @@ class Agent:
         
         return tagged
 
+    ## self.synonyms(word) returns list of synonyms for inputted word
+    ## if word is more than one word, returns list of synonyms for first word only
+    ## has error catching now
     def synonyms(self, word):
-        tag = self.pos_tag(word)
-        synonyms = []
-        for syn_set in wordnet.synsets(word):
-            for l in syn_set.lemmas():
-                name = l.name().replace("_", " ")
-                testTag = self.pos_tag(name)
-                if testTag[0][1] == tag[0][1]:
-                    name = name.lower()
-                    synonyms.append(name)
-        
-        print(set(synonyms))
+        try:
+            tag = self.pos_tag(word)
+            if " " in word:
+                tag = self.pos_tag(word.split()[0])
+            synonyms = []
+            for syn_set in wordnet.synsets(word):
+                for l in syn_set.lemmas():
+                    name = l.name().replace("_", " ")
+                    testTag = self.pos_tag(name)
+                    if testTag[0][1] == tag[0][1]:
+                        name = name.lower()
+                        synonyms.append(name)
+            
+            print(set(synonyms))
 
-        return synonyms
+            return synonyms
+        catch:
+            print("Encountered an error; make sure you inputted a valid word to get synonyms.")
+            return word
