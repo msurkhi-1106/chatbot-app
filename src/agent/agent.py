aa6db47e3c116ccc999c62a8daaae55f2362ec5e
Gabriel McLachlan <75510344+GMcLachlan45@users.noreply.github.com>
2022-03-18 16:45:32 -0700

    add framework for appending base string

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -30,13 +30,32 @@ class Agent:
         #saying "hello" or "tell jessica to" or something to the front --GABE
         #TODO: COReference: Figure out if the query is about the user or their patient is talking about --Jordan C
         sentiment = self.plugins[3].parse(query)
-
+        print(ne_rec)
+        print(sentiment)
         ##TODO Sentiment for easy interchangeable sentences
-       # sentiment = self.sentiment_analysis(query)
 
         ####TODODODO: Add all of the sections, and return Dr phils smart answer to the query all 3
+        
+        base =chat(query)
 
-        return chat(query)
+        if(sentiment<-.5):
+            oh_nos = ["I'm sorry to hear that!",
+                      "That doesn't sound very good.'",
+                      "I'm sorry you feel this way.",
+                      "I hope I can help you feel better!",
+                      "Hold on, we'll get you feeling better in no time!",
+                      "I'll work my hardest to help you feel better."]
+            base = oh_nos[randint(len(oh_nos))] + base
+        if ne_rec:
+            if "I'm in base" in base:
+                print(ne_rec[0])
+            if "they" in base:
+                print("Please tell " + ne_rec + ": \"" + base + "\"")
+
+
+            
+
+        return base +" ---" +str(sentiment) 
 
     
     def pos_tag(self, query):
