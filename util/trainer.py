b92cb9dd696c8382bde2cf18306eeec819850570
Gabriel McLachlan <75510344+GMcLachlan45@users.noreply.github.com>
2022-03-19 06:49:17 -0700

    Increase epochs

--- util/trainer.py
+++ util/trainer.py
@@ -76,7 +76,7 @@ class Trainer:
         model.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
 
 
-        epochs = 225
+        epochs = 300
         history = model.fit(padded_sequences, np.array(training_labels), epochs=epochs)
 
         # saving model
