6268aa5d867aad79eb68811af1227c6fa14b69c8
Gabriel McLachlan <75510344+GMcLachlan45@users.noreply.github.com>
2022-03-19 08:23:35 -0700

    Final fix on NER

--- src/agent/plugins/entity_recognition.py
+++ src/agent/plugins/entity_recognition.py
@@ -11,7 +11,13 @@ class EntityRecognition(AgentPlugin):
                     st = ""
                     for j in range (len(i)):
                         st = st + " " + i[j][0]
-                    if(st != 'Hello' and st != 'Hey'):
-                        print(st + " is a named entity")
                         ne_set.append(st.strip())
+            if "Hello" in ne_set:
+                ne_set.pop(0)
+            if "Hi" in ne_set:
+                ne_set.pop(0)
+            if "Hey" in ne_set:
+                ne_set.pop(0)
+
+
             return ne_set
