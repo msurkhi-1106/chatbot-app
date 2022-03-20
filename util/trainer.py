f7662079c3a699353a2ce4c1d6873d7cbb97ff12
Gabriel McLachlan <75510344+GMcLachlan45@users.noreply.github.com>
2022-03-19 23:46:21 -0700

    Set the epochs to 45 (found sweet spot)

--- util/trainer.py
+++ util/trainer.py
@@ -76,7 +76,7 @@ class Trainer:
         model.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
 
 
-        epochs = 35
+        epochs = 45
         history = model.fit(padded_sequences, np.array(training_labels), epochs=epochs)
 
         # saving model
